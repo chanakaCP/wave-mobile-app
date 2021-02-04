84182e9e80522892bb443e3a2437b4ab672996ed
chanakaCP <praveenchanaka415@gmail.com>
2021-02-04 17:37:32 +0530

    ADD: custom widget - profile tab

---
+++ lib/screens/CustomWidgets/CustomProfileTabContainer.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:wave_mobile_app/Shared/SizeConfig.dart';
+import 'package:wave_mobile_app/screens/CustomWidgets/CustomText.dart';
+
+// ignore: must_be_immutable
+class CustomProfileTabContainer extends StatelessWidget {
+  String title;
+  final VoidCallback callback;
+  CustomProfileTabContainer({@required this.title, @required this.callback});
+
+  double blockHeight = SizeConfig.safeBlockVertical;
+  double blockWidth = SizeConfig.safeBlockHorizontal;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: blockWidth * 7.5,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: CustomText(
+                text: title,
+                color: Colors.black,
+                size: blockHeight * 2,
+                weight: FontWeight.w400,
+              ),
+            ),
+            IconButton(
+              padding: EdgeInsets.only(bottom: blockHeight),
+              color: Colors.black,
+              icon: Icon(
+                Icons.keyboard_arrow_right,
+                size: blockHeight * 3,
+              ),
+              onPressed: () {
+                callback();
+              },
+            )
+          ],
+        ),
+      ),
+      onTap: () {
+        callback();
+      },
+    );
+  }
+}
