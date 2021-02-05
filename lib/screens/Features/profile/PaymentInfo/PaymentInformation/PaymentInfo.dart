7197bffe0538a78115b897f9a427b7b25f5799fd
ShanManage <lakshan2015dk@gmail.com>
2021-02-05 16:58:01 +0530

    add custom page view

--- lib/screens/Features/profile/PaymentInfo/PaymentInformation/PaymentInfo.dart
+++ lib/screens/Features/profile/PaymentInfo/PaymentInformation/PaymentInfo.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:wave_mobile_app/Services/AuthService.dart';
 import 'package:wave_mobile_app/Shared/SizeConfig.dart';
+import 'package:wave_mobile_app/screens/CustomWidgets/CustomPageView.dart';
 import 'package:wave_mobile_app/screens/CustomWidgets/CustomText.dart';
 import 'AddCardDetails/AddCardDetailsView.dart';
 import 'MyCardDetails/MyCardDetailesView.dart';
@@ -25,69 +27,63 @@ class _PaymentTabViewScreenState extends State<PaymentTabViewScreen>
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          actions: [
-            RaisedButton(
-              onPressed: () async {
-                await authService.signOut();
-              },
-              child: Text("LOGOUT"),
-            )
-          ],
-        ),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.symmetric(vertical: blockHeight * 0.5),
-            width: double.infinity,
-            child: Column(
-              children: [
-                Container(
-                  height: blockHeight * 6,
-                  width: double.infinity,
-                  child: TabBar(
-                    unselectedLabelColor: Colors.black,
-                    labelColor: Colors.black,
-                    indicatorColor: Colors.black,
-                    indicatorWeight: blockWidth * 0.5,
-                    controller: _tabController,
-                    isScrollable: true,
-                    indicatorSize: TabBarIndicatorSize.label,
-                    labelPadding:
-                        EdgeInsets.symmetric(horizontal: blockHeight * 8.75),
-                    tabs: [
-                      Tab(
-                        child: CustomText(
-                          text: "Card List",
-                          size: blockWidth * 4,
-                          weight: FontWeight.w400,
-                          color: Colors.black,
-                        ),
+    return CustomPageView(
+      callbackHead: () {
+        Get.back();
+      },
+      callbackTail: () async {
+        await authService.signOut();
+      },
+      childWidget: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: blockHeight * 0.5),
+          width: double.infinity,
+          child: Column(
+            children: [
+              Container(
+                height: blockHeight * 6,
+                width: double.infinity,
+                child: TabBar(
+                  unselectedLabelColor: Colors.black,
+                  labelColor: Colors.black,
+                  indicatorColor: Colors.black,
+                  indicatorWeight: blockWidth * 0.5,
+                  controller: _tabController,
+                  isScrollable: true,
+                  indicatorSize: TabBarIndicatorSize.label,
+                  labelPadding:
+                      EdgeInsets.symmetric(horizontal: blockHeight * 8.75),
+                  tabs: [
+                    Tab(
+                      child: CustomText(
+                        text: "Card List",
+                        size: blockWidth * 4,
+                        weight: FontWeight.w400,
+                        color: Colors.black,
                       ),
-                      Tab(
-                        child: CustomText(
-                          text: "Add Card",
-                          size: blockWidth * 4,
-                          weight: FontWeight.w400,
-                          color: Colors.black,
-                        ),
+                    ),
+                    Tab(
+                      child: CustomText(
+                        text: "Add Card",
+                        size: blockWidth * 4,
+                        weight: FontWeight.w400,
+                        color: Colors.black,
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                Container(
-                  height: blockHeight * 88,
-                  child: TabBarView(
-                    controller: _tabController,
-                    children: [
-                      MyCardDetailsView(),
-                      AddCardDetailsView(),
-                    ],
-                  ),
+              ),
+              Container(
+                height: blockHeight * 88,
+                child: TabBarView(
+                  controller: _tabController,
+                  children: [
+                    MyCardDetailsView(),
+                    AddCardDetailsView(),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
--- lib/screens/Features/profile/PersonalInformation/ChangePassword/ChangePasswordView.dart
+++ lib/screens/Features/profile/PersonalInformation/ChangePassword/ChangePasswordView.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:stacked/stacked.dart';
 import 'package:wave_mobile_app/Shared/SizeConfig.dart';
 import 'package:wave_mobile_app/screens/CustomWidgets/CustomButton.dart';
 import 'package:wave_mobile_app/screens/CustomWidgets/CustomFormField.dart';
 import 'package:wave_mobile_app/screens/CustomWidgets/CustomLoading.dart';
+import 'package:wave_mobile_app/screens/CustomWidgets/CustomPageView.dart';
 import 'package:wave_mobile_app/screens/CustomWidgets/CustomText.dart';
 import 'ChangePasswordViewModel.dart';
 
@@ -17,80 +19,72 @@ class ChangePasswordView extends StatelessWidget {
     return ViewModelBuilder<ChangePasswordViewModel>.reactive(
       viewModelBuilder: () => ChangePasswordViewModel(),
       builder: (_, model, child) {
-        return SafeArea(
-          child: Scaffold(
-            appBar: AppBar(
-              actions: [
-                RaisedButton(
-                  onPressed: () async {
-                    await model.authService.signOut();
-                  },
-                  child: Text("LOGOUT"),
-                )
-              ],
-            ),
-            body: SingleChildScrollView(
-              child: Container(
-                padding: EdgeInsets.symmetric(
-                  vertical: blockHeight * 7.5,
-                  horizontal: blockWidth * 10,
-                ),
-                child: Form(
-                  key: model.formKey,
-                  child: Column(
-                    children: [
-                      CustomFormField(
-                        hintText: "old password",
-                        isPass: true,
-                        fieldController: model.oldPassController,
-                        prefixIcon: Icons.lock,
-                      ),
-                      SizedBox(height: blockHeight * 2.5),
-                      CustomFormField(
-                        hintText: "new password",
-                        isPass: true,
-                        fieldController: model.passController,
-                        prefixIcon: Icons.lock,
-                      ),
-                      SizedBox(height: blockHeight * 2.5),
-                      CustomFormField(
-                        hintText: "confirm new password",
-                        isPass: true,
-                        fieldController: model.confirmPassController,
-                        prefixIcon: Icons.lock,
-                      ),
-                      SizedBox(height: blockHeight * 2.5),
-                      CustomButton(
-                        width: blockWidth * 60,
-                        height: blockHeight * 6,
-                        title: "Change Password",
-                        bgColor: Colors.blue[800],
-                        textColor: Colors.white,
-                        callback: () {
-                          model.onClickChangePassword();
-                        },
-                      ),
-                      SizedBox(height: blockHeight * 1.5),
-                      (model.isError == true)
-                          ? CustomText(
-                              text: model.error,
-                              color: Colors.red,
-                              size: blockWidth * 4,
-                            )
-                          : Container(),
-                      (model.isSuccess == true)
-                          ? CustomText(
-                              text: "Successfully changed password",
-                              color: Colors.green,
-                              size: blockWidth * 4,
-                            )
-                          : Container(),
-                      SizedBox(height: blockHeight * 3.5),
-                      (model.isLoading == false)
-                          ? Container()
-                          : CustomLoading(),
-                    ],
-                  ),
+        return CustomPageView(
+          callbackHead: () {
+            Get.back();
+          },
+          callbackTail: () async {
+            await model.authService.signOut();
+          },
+          childWidget: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                vertical: blockHeight * 7.5,
+                horizontal: blockWidth * 10,
+              ),
+              child: Form(
+                key: model.formKey,
+                child: Column(
+                  children: [
+                    CustomFormField(
+                      hintText: "old password",
+                      isPass: true,
+                      fieldController: model.oldPassController,
+                      prefixIcon: Icons.lock,
+                    ),
+                    SizedBox(height: blockHeight * 2.5),
+                    CustomFormField(
+                      hintText: "new password",
+                      isPass: true,
+                      fieldController: model.passController,
+                      prefixIcon: Icons.lock,
+                    ),
+                    SizedBox(height: blockHeight * 2.5),
+                    CustomFormField(
+                      hintText: "confirm new password",
+                      isPass: true,
+                      fieldController: model.confirmPassController,
+                      prefixIcon: Icons.lock,
+                    ),
+                    SizedBox(height: blockHeight * 2.5),
+                    CustomButton(
+                      width: blockWidth * 60,
+                      height: blockHeight * 6,
+                      title: "Change Password",
+                      bgColor: Colors.blue[800],
+                      textColor: Colors.white,
+                      callback: () {
+                        model.onClickChangePassword();
+                      },
+                    ),
+                    SizedBox(height: blockHeight * 1.5),
+                    (model.isError == true)
+                        ? CustomText(
+                            text: model.error,
+                            color: Colors.red,
+                            size: blockWidth * 4,
+                          )
+                        : Container(),
+                    (model.isSuccess == true)
+                        ? CustomText(
+                            text: "Successfully changed password",
+                            color: Colors.green,
+                            size: blockWidth * 4,
+                          )
+                        : Container(),
+                    SizedBox(height: blockHeight * 3.5),
+                    (model.isLoading == false) ? Container() : CustomLoading(),
+                  ],
                 ),
               ),
             ),
