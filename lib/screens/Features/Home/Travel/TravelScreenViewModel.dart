cb6d881633d98a126bfbf53b0a8d8398d97c520d
ShanManage <lakshan2015dk@gmail.com>
2021-02-13 13:37:40 +0530

    add: google map feature

--- lib/screens/Features/Home/Travel/TravelScreenViewModel.dart
+++ lib/screens/Features/Home/Travel/TravelScreenViewModel.dart
@@ -3,4 +3,23 @@ import 'package:wave_mobile_app/Services/AuthService.dart';
 
 class TravelScreenViewModel extends ChangeNotifier {
   final AuthService authService = AuthService();
+
+//   final List<GoogleMapExampleAppPage> _allPages = <GoogleMapExampleAppPage>[
+//   MapUiPage(),
+//   MapCoordinatesPage(),
+//   MapClickPage(),
+//   AnimateCameraPage(),
+//   MoveCameraPage(),
+//   PlaceMarkerPage(),
+//   MarkerIconsPage(),
+//   ScrollingMapPage(),
+//   PlacePolylinePage(),
+//   PlacePolygonPage(),
+//   PlaceCirclePage(),
+//   PaddingPage(),
+//   SnapshotPage(),
+//   LiteModePage(),
+//   TileOverlayPage(),
+// ];
+
 }
